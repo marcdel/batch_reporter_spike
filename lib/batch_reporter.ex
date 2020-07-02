3e65c2b2a386f8b5b31a97eeefed2d2280672d0e
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-07-02 10:37:27 -0700

    Adds ability to configure the max events send in a batch

--- lib/batch_reporter.ex
+++ lib/batch_reporter.ex
@@ -1,11 +1,18 @@
 defmodule BatchReporter do
   use GenServer
 
-  @default_state %{report_scheduled: false, unreported_events: []}
+  @max_events 1000
+
+  defstruct [:report_fn, :max_events, report_scheduled: false, unreported_events: []]
 
   def start_link(opts) do
     {report_fn, opts} = Keyword.pop(opts, :report_fn, &do_report_events/1)
-    state = Map.put(@default_state, :report_fn, report_fn)
+    {max_events, opts} = Keyword.pop(opts, :max_events, @max_events)
+
+    state =
+      %__MODULE__{}
+      |> Map.put(:report_fn, report_fn)
+      |> Map.put(:max_events, max_events)
 
     GenServer.start_link(__MODULE__, state, opts)
   end
@@ -28,12 +35,16 @@ defmodule BatchReporter do
   end
 
   def handle_info(:report_events, state) do
-    state.report_fn.(state.unreported_events)
+    {events_to_send, remaining_events} =
+      Enum.split(state.unreported_events, state.max_events)
+
+    state.report_fn.(events_to_send)
 
     updated_state =
       state
       |> reset_report_scheduled()
-      |> clear_unreported_events()
+      |> maybe_report_events()
+      |> set_unreported_events(remaining_events)
 
     {:noreply, updated_state}
   end
@@ -42,8 +53,8 @@ defmodule BatchReporter do
     %{state | unreported_events: state.unreported_events ++ [event]}
   end
 
-  defp clear_unreported_events(state) do
-    %{state | unreported_events: []}
+  defp set_unreported_events(state, remaining_events) do
+    %{state | unreported_events: remaining_events}
   end
 
   defp set_report_scheduled(state) do
@@ -54,9 +65,9 @@ defmodule BatchReporter do
     %{state | report_scheduled: false}
   end
 
-  defp maybe_report_events(%{unreported_events: []} = state), do: state
-
   defp maybe_report_events(%{report_scheduled: true} = state), do: state
+
+  defp maybe_report_events(%{unreported_events: []} = state), do: state
 
   defp maybe_report_events(state) do
     send(self(), :report_events)
--- test/batch_reporter_test.exs
+++ test/batch_reporter_test.exs
@@ -6,22 +6,27 @@ defmodule BatchReporterTest do
     test_pid = self()
 
     report_fn = fn events ->
+      IO.inspect({:reported, events})
+
       Enum.each(events, fn event ->
         send(test_pid, {:reported, event})
       end)
 
-      IO.inspect({:reported, events})
+      # Wait longer the more events queued up.
+      # In reality this should be more like O(1) than O(n), but it makes for a nice demonstration.
       Process.sleep(100 * length(events))
     end
 
-    {:ok, pid} = BatchReporter.start_link(report_fn: report_fn, name: :yolo)
+    {:ok, pid} = BatchReporter.start_link(report_fn: report_fn, max_events: 5, name: :yolo)
 
     Enum.each(1..100, fn event_id ->
       event = %{id: event_id}
       BatchReporter.enqueue_event(pid, event)
+
+      # Send events faster and faster
       Process.sleep(100 - event_id)
     end)
 
-    assert_receive {:reported, %{id: 100}}, 1000
+    assert_receive {:reported, %{id: 100}}, 10_000
   end
 end
